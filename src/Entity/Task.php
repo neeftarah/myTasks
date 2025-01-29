7dd33f7c275e9879fe33da1e7f487b6138032c88
Jérémy MOREAU <morea_j35@auditeur.cnam.fr>
2025-01-29 18:00:49 +0100

    Close #6 : Valider les données des tâches

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -5,16 +5,33 @@ namespace App\Entity;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Enum\TaskPriority;
 use App\Enum\TaskStatus;
 use App\Repository\TaskRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 #[ORM\HasLifecycleCallbacks]
-#[ApiResource]
+#[ApiResource(
+    shortName: 'Tasks',
+    description: 'Vos tâches à réaliser',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(),
+        new Put(),
+        new Patch(),
+        new Delete(),
+    ],
+)]
 #[GetCollection(paginationMaximumItemsPerPage: 10)]
 #[ApiFilter(OrderFilter::class, properties: ["dueDate" => "ASC", "priority" => "DESC"])]
 class Task
@@ -25,12 +42,20 @@ class Task
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: 'Le titre est obligatoire')]
+    #[Assert\Length(
+        min: 10,
+        max: 255,
+        minMessage: 'Votre titre doit faire plus de 10 caractères !',
+        maxMessage: 'Votre titre doit faire moins de 255 caractères !'
+    )]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+//    #[Assert\DateTime(format: 'Y-m-d', message: 'La date doit être au format YYYY-MM-DD')]
     private ?\DateTimeInterface $dueDate = null;
 
     #[ORM\Column(enumType: TaskStatus::class)]
